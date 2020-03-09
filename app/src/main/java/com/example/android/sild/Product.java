098bf25541889ff7a4723363f71b27db037430b8
KetyaNop <ketya.n@ligercambodia.org>
2020-03-09 22:42:12 +0700

    Create Product.java

---
+++ app/src/main/java/com/example/android/sild/Product.java
@@ -0,0 +1,32 @@
+package com.example.android.sild;
+
+import android.location.Location;
+import android.media.Image;
+
+public abstract class Product {
+    String barcode;
+    Location location;
+    Image image;
+
+    public Product(String bar, Location loc, Image img){
+        barcode = bar;
+        location = loc;
+        img = image;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+}
